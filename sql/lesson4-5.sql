f0fff232e621f3149ed3fbb1c8cdc3cd31eb4930
wan3wan <benwan1228@gmail.com>
2024-10-13 16:11:35 +0800

    4-11

--- sql/lesson4-5.sql
+++ sql/lesson4-5.sql
@@ -9,3 +9,27 @@ having sum(amount) > 200;
 SELECT store_id ,COUNT(customer_id) AS 數量
 FROM customer
 GROUP BY store_id;
+
+/*subquery*/
+SELECT *
+FROM country;
+
+SELECT country_id
+FROM country
+WHERE country='Taiwan';
+
+SELECT city
+FROM city
+WHERE country_id = 92;
+
+SELECT city
+FROM city
+WHERE country_id = (
+	SELECT country_id
+	FROM country
+	WHERE country='Taiwan'
+);
+
+SELECT city
+FROM city JOIN country ON city.country_id = country.country_id
+WHERE country = 'Taiwan';

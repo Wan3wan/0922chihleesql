03ba380392b3897d6eb721f7ea7aadf85517ef95
wan3wan <benwan1228@gmail.com>
2024-11-03 10:17:37 +0800

    lesson7

--- lesson7/lesson7_1.sql
+++ lesson7/lesson7_1.sql
@@ -1,14 +1,21 @@
+CREATE TABLE IF NOT EXISTS 市場(
+ name VARCHAR(20),
+ country varchar(20),
+ primary key(name)
+);
+
 CREATE TABLE IF NOT EXISTS 股市(
-	stock_id SERIAL,
-	date DATE,
-	open NUMERIC(17,10),
-	high NUMERIC(17,10),
-	low NUMERIC(17,16),
-	close NUMERIC(17,16),
-	adj_close NUMERIC(17,10),
-	volume BIGINT DEFAULT 0,
-	name VARCHAR(20),
-	PRIMARY KEY(stock_id),
-	FOREIGN KEY(name) REFERERENCES 151](name)
-	ON DELETE NO ACTION
-	ON UPDATE CASCADE
+ stock_id SERIAL,
+ date DATE,
+ open NUMERIC(17,10),
+ high NUMERIC(17,10),
+ low NUMERIC(17,10),
+ close NUMERIC(17,10),
+ adj_close NUMERIC(17,10),
+ volume BIGINT DEFAULT 0,
+ name VARCHAR(20),
+ PRIMARY KEY(stock_id),
+ FOREIGN KEY(name) REFERENCES 市場(name) 
+ ON DELETE NO ACTION
+ ON UPDATE CASCADE
+);

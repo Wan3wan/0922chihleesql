37ef4febf54d31fc48622d0d6fc51084bf2237b0
wan3wan <benwan1228@gmail.com>
2024-11-10 11:08:28 +0800

    lesson8_3_1

--- lesson8/lesson8_2.py
+++ lesson8/lesson8_2.py
@@ -4,38 +4,35 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-conn = psycopg2.connect(host=os.environ['POSTGRE_HOST'],database=os.environ['POSTGRE_DATABASE'],user=os.environ['POSTGRE_USER'],password=os.environ['POSTGRE_PASSWORD'])
-with conn:
-    with conn.cursor() as cursor:
-        sql = '''
-        SELECT * FROM 市場
-        '''
-        cursor.execute(sql)
-        all_country = cursor.fetchall()
-conn.close()
-
-
-st.title('世界大盤分析')
-with st.sidebar:
-    st.title('請選擇股票市場:')
+@st.cache_resource
+def get_country():
+    conn = psycopg2.connect(host=os.environ['POSTGRE_HOST'],database=os.environ['POSTGRE_DATABASE'],user=os.environ['POSTGRE_USER'],password=os.environ['POSTGRE_PASSWORD'])
+    with conn:
+        with conn.cursor() as cursor:
+            sql = '''
+            SELECT * FROM 市場
+            '''
+            cursor.execute(sql)
+            all_country = cursor.fetchall()
+    conn.close()
     input_dict = dict(all_country)
-    options = st.multiselect("請選擇",input_dict.values(),default='台灣',placeholder="請選擇市場",label_visibility='hidden')
-    st.write(options)
-
+    return input_dict.values()
 
 def user_select():
     print('使用者選擇了')
     print(st.session_state.stocks)
 
+
 st.title('世界大盤分析')
 with st.sidebar:
     default_country = '台灣'
     st.title('請選擇股票市場:')
-    input_dict = dict(all_country)
-    st.multiselect("請選擇",input_dict.values(),
+    
+    st.multiselect("請選擇",get_country(),
                     default=default_country,
                     placeholder="請選擇市場",
                     label_visibility='hidden',
-                    key='stock',
+                    key='stocks',
                     on_change=user_select)
-st.write(default_country)
+    st.write(default_country)
+    

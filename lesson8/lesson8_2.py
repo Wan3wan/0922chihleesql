e6d1504bec2c55eeae6586182640accc9d13971b
wan3wan <benwan1228@gmail.com>
2024-11-10 14:59:03 +0800

    lesson8_5

--- lesson8/lesson8_2.py
+++ lesson8/lesson8_2.py
@@ -5,7 +5,8 @@ import os
 import pandas as pd
 load_dotenv()
 
-@st.cache_resource
+
+@st.cache_data
 def getData(country:tuple[str])->list[tuple]:
     conn = psycopg2.connect(host=os.environ['POSTGRE_HOST'],
                             database=os.environ['POSTGRE_DATABASE'],
@@ -40,25 +41,24 @@ def get_country():
     conn.close()
     input_dict = dict(all_country)
     return input_dict.values()
+   
+    
 
-def user_select():
-    print('使用者選擇了')
-    print(st.session_state.stocks)
 
-st.title('世界大盤分析')
 default_country = '台灣'
 with st.sidebar:    
-    st.title('請選擇股票市場:')
+    st.title('請選擇股票市場:')    
+    options:list[str] =st.multiselect("請選擇",get_country(),
+                        default=default_country,
+                        placeholder="請選擇市場",
+                        label_visibility='hidden',
+                        key='stocks'
+                        )
     
-    st.multiselect("請選擇",get_country(),
-                    default=default_country,
-                    placeholder="請選擇市場",
-                    label_visibility='hidden',
-                    key='stocks',
-                    on_change=user_select)
-    st.write(default_country)
-
-df = pd.DataFrame(getData((default_country,)),columns=['國家','代號','日期','收盤價','成交量'])
-df['收盤價'] = df['收盤價'].astype('float').round(decimals=2)
-st.line_chart(data=df,x='日期',y='收盤價',color='國家')
+selectedCountry:tuple[str] = tuple(options) 
+if selectedCountry: 
+    st.title('世界大盤分析')
+    df = pd.DataFrame(getData(selectedCountry),columns=['國家','代號','日期','收盤價','成交量'])
+    df['收盤價'] = df['收盤價'].astype('float').round(decimals=2)
+    st.line_chart(data=df,x='日期',y='收盤價',color='國家')
     

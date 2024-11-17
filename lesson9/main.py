fc068d03bfd4451df3f54b776646399dd95be4cb
wan3wan <benwan1228@gmail.com>
2024-11-17 14:11:28 +0800

    lesson9_4

--- lesson9/main.py
+++ lesson9/main.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
 import streamlit as st
-from time import sleep
-from tools.datasource import get_data
+from pprint import pprint
+from tools import get_data
+load_dotenv()
+
 
 @st.dialog("目前發生問題,請稍後再試")
 def alert():
@@ -9,6 +11,8 @@ def alert():
         st.stop()
 
 with st.spinner('下載資料中...'):
-        get_data()
-alert()
-st.write('顯示頁面')
+        youbikes = get_data()
+        
+if youbikes is None:
+    alert()
+st.write(youbikes)
